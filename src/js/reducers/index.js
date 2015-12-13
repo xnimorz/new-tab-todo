c4619d8815bb4ba381df209a6abc307e06163693
Nikita Mostovoy <xnimor@ya.ru>
2015-12-13 19:34:25 +0300

    remove timer reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,11 +1,10 @@
 import { combineReducers } from 'redux'
 import todo from './todo'
 import user from './user'
-import timer from './timer'
 import view from './view'
 
 const rootReducer = combineReducers({
-    todo, user, timer, view
+    todo, user, view
 });
 
 export default rootReducer;
